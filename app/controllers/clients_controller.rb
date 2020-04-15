8cc0417468db1d9ad0e3ea33f3d7ef6b389ff45d
Angeliano <angelo.sousa@acensjr.com>
2020-04-15 20:30:26 -0300

    Quase lá

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -10,26 +10,14 @@ class ClientsController < ApplicationController
   end
 
   def edit
- @client = Client.find(params[:id])
-      
+    @client = Client.find(params[:id])  
   end
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @client.update(client_params)
-  #       format.html { redirect_to clients_path, notice: 'Perfil atualizado com sucesso.' }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @client.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   def update
-    # @client.saldo_dotz += @client.dotz.moeda
     @client = Client.find(params[:id])
+    @client.saldo_dotz = @client.saldo_dotz + @client.valor
     respond_to do |format|
       if @client.update(client_params)
         format.html { redirect_to clients_path, notice: 'Perfil atualizado com sucesso.' }
@@ -52,7 +40,7 @@ class ClientsController < ApplicationController
     end
     
     def client_params
-      params.require(:client).permit(:nome, :email, :cpf, :saldo_dotz, dotz_attributes: [:moeda])
+      params.require(:client).permit(:nome, :email, :cpf, :saldo_dotz, :valor)
     end
 end
   
