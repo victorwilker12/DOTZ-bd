97c9bbe2cb3a1f5e41bd68c78c3bc7f9bcbbf058
Angeliano <angelo.sousa@acensjr.com>
2020-04-17 00:18:38 -0300

    Upload de imagem funcionando

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:nome, :valor_dotz, :image, :estoque)
+      params.require(:product).permit(:nome, :valor_dotz, :imagem, :estoque)
     end
 end
